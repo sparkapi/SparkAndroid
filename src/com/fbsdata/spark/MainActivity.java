f4ae42edb475001f11a377f8262187b1604d99b4
David Ragones <dragones@dreamcommerce.co>
2013-01-24 23:46:18 -0800

    scaffolding for LoginActivity

---
+++ src/com/fbsdata/spark/MainActivity.java
@@ -0,0 +1,25 @@
+package com.fbsdata.spark;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
